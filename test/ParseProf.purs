19db28330b602516e4c9c7bec27d0d5a12a5c76a
ebenpack <ebenpackwood@gmail.com>
2019-03-16 18:26:35 -0400

    Add moar tests

--- test/ParseProf.purs
+++ test/ParseProf.purs
@@ -2,76 +2,140 @@ module Test.Main where
 
 import Prelude
 
-import Data.Date as Date
-import Data.DateTime as DateTime
 import Data.Either (Either(..))
-import Data.Enum (toEnum)
 import Data.List (List, fromFoldable)
-import Data.Maybe (fromJust)
-import Data.Time as Time
+import Data.Maybe
 import Effect (Effect)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
-import ParseProf as P
-import TestData (costCenterStackCosts)
+import ParseProf as ParseProf
 import Partial.Unsafe (unsafePartial)
+import Prof as Prof
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
+import TestData ( dateTime,
+    simplePerCostCenterCostsColumnWidth, detailedPerCostCenterCostsColumnWidth,
+    simpleCostCenterStackCostsColumnWidth, detailedCostCenterStackCostsColumnWidth,
+    simpleCostCenterStackCosts, detailedCostCenterStackCosts,
+    simplePerCostCenterCosts, detailedPerCostCenterCosts,
+    simplePerCostCenterCostsLine, detailedPerCostCenterCostsLine,
+    simpleCostCenterStackCostsLine, detailedCostCenterStackCostsLine
+    )
 import Text.Parsing.StringParser (runParser)
 
-dateTime :: DateTime.DateTime
-dateTime =
-    let d1 = unsafePartial fromJust $ Date.canonicalDate <$> toEnum 2019 <*> pure Date.March <*> toEnum 11
-        t1 = unsafePartial $ fromJust $ Time.Time <$> toEnum 7 <*> toEnum 33 <*> toEnum 0 <*> toEnum 0
-        dt1 = DateTime.DateTime d1 t1
-    in dt1
-
-perCostCenterCosts :: List P.PerCostCenterCosts
-perCostCenterCosts = fromFoldable 
-    [ { name: "MAIN", mod: "MAIN", src: "<built-in>", time: 0.0, alloc: 68.8 }
-    , { name: "CAF" , mod: "GHC.IO.Handle.FD" , src: "<entire-module>" , time: 0.0 , alloc: 21.1 }
-    , { name: "CAF", mod: "GHC.IO.Encoding", src: "<entire-module>", time: 0.0, alloc: 1.7 }
-    , { name: "main", mod: "Main", src: "app/Main.hs:(77,1)-(80,27)", time: 0.0, alloc: 6.2 }
-    ]
-        
 test_parseDateTime :: Spec Unit
 test_parseDateTime = describe "parseDateTime" $ do
-    it "does thing" $ do
-        let actual = runParser P.parseDateTime "Mon Mar 11 07:33 2019"
+    it "parses datetime" $ do
+        let actual = runParser ParseProf.parseDateTime "Mon Mar 11 07:33 2019"
         actual `shouldEqual` (Right dateTime)
 
 test_parseInteger :: Spec Unit
-test_parseInteger = describe "parseDateTime" $ do
-    it "does thing" $ do
-        let actual = runParser P.parseInteger "1234"
+test_parseInteger = describe "parseInteger" $ do
+    it "parses integer" $ do
+        let actual = runParser ParseProf.parseInteger "1234"
         actual `shouldEqual` (Right 1234)
 
 test_parseIntegerWithCommas :: Spec Unit
-test_parseIntegerWithCommas = describe "commas" $ do
-    it "does thing" $ do
-        let actual = runParser P.parseIntegerWithCommas "12,345"
+test_parseIntegerWithCommas = describe "parseIntegerWithCommas" $ do
+    it "parses integers with comma separator" $ do
+        let actual = runParser ParseProf.parseIntegerWithCommas "12,345"
         actual `shouldEqual` (Right 12345)
 
+test_parseCostCenterCostHeader :: Spec Unit
+test_parseCostCenterCostHeader = do
+    it "parses simple cost center headers" $ do
+        let header = "COST CENTRE MODULE           SRC                         %time %alloc"
+            actual = runParser ParseProf.parseCostCenterCostHeader header
+        actual `shouldEqual` (Right simplePerCostCenterCostsColumnWidth)
+    it "parses detailed cost center headers" $ do
+        let header = "COST CENTRE MODULE           SRC                         %time %alloc  ticks     bytes"
+            actual = runParser ParseProf.parseCostCenterCostHeader header
+        actual `shouldEqual` (Right detailedPerCostCenterCostsColumnWidth)
+
+test_parseCostCenterStackHeader :: Spec Unit
+test_parseCostCenterStackHeader = do
+    it "parses simple cost center stack headers" $ do
+        let header = "COST CENTRE  MODULE                SRC                        no.      entries  %time %alloc   %time %alloc"
+            actual = runParser ParseProf.parseCostCenterStackHeader header
+        actual `shouldEqual` (Right simpleCostCenterStackCostsColumnWidth)
+    it "parses detailed cost center stack headers" $ do
+        let header = "COST CENTRE  MODULE                SRC                        no.      entries  %time %alloc   %time %alloc  ticks     bytes"
+            actual = runParser ParseProf.parseCostCenterStackHeader header
+        actual `shouldEqual` (Right detailedCostCenterStackCostsColumnWidth)
+
+test_parsePerCostCenterCostsLine :: Spec Unit
+test_parsePerCostCenterCostsLine = do
+    it "parses simple cost center cost line" $ do
+        let line = "main        Main             app/Main.hs:(77,1)-(80,27)    0.0    6.2"
+            actual = runParser (ParseProf.parsePerCostCenterCostsLine simplePerCostCenterCostsColumnWidth) line
+        actual `shouldEqual` (Right simplePerCostCenterCostsLine)
+    it "parses detailed cost center cost line" $ do
+        let line = "main        Main             app/Main.hs:(77,1)-(80,27)    0.0    6.2      0     10272"
+            actual = runParser (ParseProf.parsePerCostCenterCostsLine detailedPerCostCenterCostsColumnWidth) line
+        actual `shouldEqual` (Right detailedPerCostCenterCostsLine)
+
+test_parseCostCenterStackLine :: Spec Unit
+test_parseCostCenterStackLine = do
+    it "parses simple cost center stack cost line" $ do
+        let line = "     fib'.go Main                  app/Main.hs:(72,5)-(74,51) 2492         31    0.0    0.6     0.0    0.6"
+            actual = runParser (ParseProf.parseCostCenterStackLine simpleCostCenterStackCostsColumnWidth) line
+        actual `shouldEqual` (Right { stack: simpleCostCenterStackCostsLine, depth: 5 })
+    it "parses detailed cost center stack cost line" $ do
+        let line = "     fib'.go Main                  app/Main.hs:(72,5)-(74,51) 2492         31    0.0    0.6     0.0    0.6      0       928"
+            actual = runParser (ParseProf.parseCostCenterStackLine detailedCostCenterStackCostsColumnWidth) line
+        actual `shouldEqual` (Right { stack: detailedCostCenterStackCostsLine, depth: 5 })
+
 test_parseProfFile :: String -> Spec Unit
-test_parseProfFile f = describe "parseDateTime" $ do
-    it "does thing" $ do
-        let actual = runParser P.parseProfFile f
+test_parseProfFile f = describe "parseProfFile" $ do
+    it "parses a prof file" $ do
+        let actual = runParser ParseProf.parseProfFile f
         actual `shouldEqual` 
             (Right { timestamp : dateTime
                     , title : "hasktest exe +RTS -N -p -RTS"
                     , totalTime : { time : 0.0, ticks : 0, interval : 1000, processors : 6 }
                     , totalAlloc : 164784
-                    , perCostCenterCosts: perCostCenterCosts
-                    , costCenterStack: costCenterStackCosts
+                    , perCostCenterCosts: simplePerCostCenterCosts
+                    , costCenterStack: simpleCostCenterStackCosts
                     })
+
+test_parseDetailedProfFile :: String -> Spec Unit
+test_parseDetailedProfFile f = describe "parseDetailedProfFile" $ do
+    it "parses a detailed prof file" $ do
+        let actual = runParser ParseProf.parseProfFile f
+        actual `shouldEqual`
+            (Right { timestamp : dateTime
+                    , title : "hasktest exe +RTS foo +RTS -N -p -P -RTS"
+                    , totalTime : { time : 0.0, ticks : 2, interval : 1000, processors : 6 }
+                    , totalAlloc : 164528
+                    , perCostCenterCosts: detailedPerCostCenterCosts
+                    , costCenterStack: detailedCostCenterStackCosts
+                    })
+
+test_takeN :: Spec Unit
+test_takeN = describe "takeN" $ do
+    it "works" $ do
+        let actual = runParser (ParseProf.takeN 6 ParseProf.justInt) "1234567890"
+        actual `shouldEqual` (Right 123456)
+    it "works" $ do
+        let takeTwo = (ParseProf.takeN 6 ParseProf.justInt >>= (\m ->
+            ParseProf.takeN 6 ParseProf.justInt >>= (\n -> pure $ m + n)))
+        let actual = runParser takeTwo "12345678901"
+        actual `shouldEqual` (Right 912468)
 
 main :: Effect Unit
 main = do
     testFile <- readTextFile UTF8 "test/hasktest-exe.prof"
+    testDetailedFile <- readTextFile UTF8 "test/hasktest-exe-detailed.prof"
     run [consoleReporter] do
-        test_parseInteger
+        -- test_takeN
+        -- test_parseInteger
         test_parseIntegerWithCommas
         test_parseDateTime
+        test_parseCostCenterCostHeader
+        test_parseCostCenterStackHeader
+        test_parsePerCostCenterCostsLine
+        test_parseCostCenterStackLine
         test_parseProfFile testFile
+        test_parseDetailedProfFile testDetailedFile
--- test/TestData.purs
+++ test/TestData.purs
@@ -1,11 +1,66 @@
 module TestData where
 
-import Data.List (List(..), (:))
-import Data.Maybe (Maybe(..))
-import ParseProf (CostCenterStackCosts, Forest, Tree(..))
+import Prelude
 
-costCenterStackCosts :: Forest CostCenterStackCosts
-costCenterStackCosts =
+import Data.Date as Date
+import Data.DateTime as DateTime
+import Data.Enum (toEnum)
+import Data.List (List(..), (:), fromFoldable)
+import Data.Maybe (Maybe(..), fromJust)
+import Data.Time as Time
+import Partial.Unsafe (unsafePartial)
+import Prof (CostCenterStackCosts, PerCostCenterCosts, Forest, Tree(..))
+import ParseProf (PerCostCenterCostsColumnWidths, CostCenterStackColumnWidths)
+
+dateTime :: DateTime.DateTime
+dateTime =
+    let d1 = unsafePartial fromJust $ Date.canonicalDate <$> toEnum 2019 <*> pure Date.March <*> toEnum 11
+        t1 = unsafePartial $ fromJust $ Time.Time <$> toEnum 7 <*> toEnum 33 <*> toEnum 0 <*> toEnum 0
+        dt1 = DateTime.DateTime d1 t1
+    in dt1
+
+simplePerCostCenterCosts :: List PerCostCenterCosts
+simplePerCostCenterCosts = fromFoldable 
+    [ { name: "MAIN", mod: "MAIN", src: "<built-in>", time: 0.0, alloc: 68.8, ticks: Nothing, bytes: Nothing }
+    , { name: "CAF" , mod: "GHC.IO.Handle.FD" , src: "<entire-module>" , time: 0.0 , alloc: 21.1, ticks: Nothing, bytes: Nothing }
+    , { name: "CAF", mod: "GHC.IO.Encoding", src: "<entire-module>", time: 0.0, alloc: 1.7, ticks: Nothing, bytes: Nothing }
+    , { name: "main", mod: "Main", src: "app/Main.hs:(77,1)-(80,27)", time: 0.0, alloc: 6.2, ticks: Nothing, bytes: Nothing }
+    ]
+
+detailedPerCostCenterCosts :: List PerCostCenterCosts
+detailedPerCostCenterCosts = fromFoldable 
+    [ { name: "MAIN", mod: "MAIN", src: "<built-in>", time: 100.0, alloc: 68.8, ticks: Just 2, bytes: Just 113160 }
+    , { name: "CAF" , mod: "GHC.IO.Handle.FD" , src: "<entire-module>" , time: 0.0 , alloc: 21.1, ticks: Just 0, bytes: Just 34704 }
+    , { name: "CAF", mod: "GHC.IO.Encoding", src: "<entire-module>", time: 0.0, alloc: 1.7, ticks: Just 0, bytes: Just 2768 }
+    , { name: "main", mod: "Main", src: "app/Main.hs:(77,1)-(80,27)", time: 0.0, alloc: 6.2, ticks: Just 0, bytes: Just 10272 }
+    ]
+
+simplePerCostCenterCostsColumnWidth :: PerCostCenterCostsColumnWidths
+simplePerCostCenterCostsColumnWidth = { name: 12, mod: 17, src: 28, time: 5, alloc: 7, ticks: 0, bytes: 0 }
+
+simplePerCostCenterCostsLine :: PerCostCenterCosts
+simplePerCostCenterCostsLine = { name: "main", mod: "Main", src: "app/Main.hs:(77,1)-(80,27)", time: 0.0, alloc: 6.2, ticks: Nothing, bytes: Nothing }
+
+detailedPerCostCenterCostsColumnWidth :: PerCostCenterCostsColumnWidths
+detailedPerCostCenterCostsColumnWidth = { name: 12, mod: 17, src: 28, time: 5, alloc: 7, ticks: 7, bytes: 10 }
+
+detailedPerCostCenterCostsLine :: PerCostCenterCosts
+detailedPerCostCenterCostsLine = { name: "main", mod: "Main", src: "app/Main.hs:(77,1)-(80,27)", time: 0.0, alloc: 6.2, ticks: Just 0, bytes: Just 10272 }
+
+simpleCostCenterStackCostsColumnWidth :: CostCenterStackColumnWidths
+simpleCostCenterStackCostsColumnWidth = { name: 13, mod: 22, src: 27, number: 9, entries: 7, individual: { time: 7, alloc: 7 }, inherited: { time: 8, alloc: 7 }, ticks: 0, bytes: 0 }
+
+simpleCostCenterStackCostsLine :: CostCenterStackCosts
+simpleCostCenterStackCostsLine = { name: "fib'.go", mod: "Main", src: "app/Main.hs:(72,5)-(74,51)", number: 2492, entries: 31, individual: { alloc: 0.6, time: 0.0 }, inherited: { alloc: 0.6, time: 0.0 }, ticks: Nothing, bytes: Nothing }
+
+detailedCostCenterStackCostsColumnWidth :: CostCenterStackColumnWidths
+detailedCostCenterStackCostsColumnWidth = { name: 13, mod: 22, src: 27, number: 9, entries: 7, individual: { time: 7, alloc: 7 }, inherited: { time: 8, alloc: 7 }, ticks: 7, bytes: 10 }
+
+detailedCostCenterStackCostsLine :: CostCenterStackCosts
+detailedCostCenterStackCostsLine = { name: "fib'.go", mod: "Main", src: "app/Main.hs:(72,5)-(74,51)", number: 2492, entries: 31, individual: { alloc: 0.6, time: 0.0 }, inherited: { alloc: 0.6, time: 0.0 }, ticks: Just 0, bytes: Just 928 }
+
+simpleCostCenterStackCosts :: Forest CostCenterStackCosts
+simpleCostCenterStackCosts =
     ( Node 
         { value: { bytes: Nothing, entries: 0, individual: { alloc: 68.8, time: 0.0 }, inherited: { alloc: 100.0, time: 0.0 }, mod: "MAIN", name: "MAIN", number: 1242, src: "<built-in>", ticks: Nothing }
         , children:
@@ -93,6 +148,103 @@ costCenterStackCosts =
                 }
             : Node
                 { value: { bytes: Nothing, entries: 0, individual: { alloc: 6.0, time: 0.0 }, inherited: { alloc: 6.0, time: 0.0 }, mod: "Main", name: "main", number: 2485, src: "app/Main.hs:(77,1)-(80,27)", ticks: Nothing }
+                , children: Nil
+                }
+            : Nil
+            )
+        }
+    : Nil
+    )
+
+detailedCostCenterStackCosts :: Forest CostCenterStackCosts
+detailedCostCenterStackCosts =
+    ( Node 
+        { value: { bytes: Just 113160, entries: 0, individual: { alloc: 68.8, time: 100.0 }, inherited: { alloc: 100.0, time: 100.0 }, mod: "MAIN", name: "MAIN", number: 1242, src: "<built-in>", ticks: Just 2 }
+        , children:
+            ( Node
+                { value: { bytes: Just 640, entries: 0, individual: { alloc: 0.4, time: 0.0 }, inherited: { alloc: 0.4, time: 0.0 }, mod: "GHC.Conc.Signal", name: "CAF", number: 1341, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 2768, entries: 0, individual: { alloc: 1.7, time: 0.0 }, inherited: { alloc: 1.7, time: 0.0 }, mod: "GHC.IO.Encoding", name: "CAF", number: 1323, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 200, entries: 0, individual: { alloc: 0.1, time: 0.0 }, inherited: { alloc: 0.1, time: 0.0 }, mod: "GHC.IO.Encoding.Iconv", name: "CAF", number: 1321, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 34704, entries: 0, individual: { alloc: 21.1, time: 0.0 }, inherited: { alloc: 21.1, time: 0.0 }, mod: "GHC.IO.Handle.FD", name: "CAF", number: 1312, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 88, entries: 0, individual: { alloc: 0.1, time: 0.0 }, inherited: { alloc: 0.1, time: 0.0 }, mod: "GHC.IO.Handle.Text", name: "CAF", number: 1310, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 48, entries: 0, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.0, time: 0.0 }, mod: "GHC.Event.Poll", name: "CAF", number: 1259, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 1272, entries: 0, individual: { alloc: 0.8, time: 0.0 }, inherited: { alloc: 0.8, time: 0.0 }, mod: "GHC.Event.Thread", name: "CAF", number: 1258, src: "<entire-module>", ticks: Just 0 }
+                , children: Nil
+                }
+            : Node
+                { value: { bytes: Just 0, entries: 0, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.0, time: 0.0 }, mod: "Main", name: "CAF:main", number: 2463, src: "app/Main.hs:77:1-4", ticks: Just 0 }
+                , children:
+                    ( Node
+                        { value: { bytes: Just 48, entries: 1, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.0, time: 0.0 }, mod: "Main", name: "main", number: 2484, src: "app/Main.hs:(77,1)-(80,27)", ticks: Just 0 }
+                        , children: Nil
+                        }
+                    : Nil
+                    )
+                }
+            : Node
+                { value: { bytes: Just 0, entries: 0, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 1.0, time: 0.0 }, mod: "Main", name: "CAF:main1", number: 2462, src: "<no location info>", ticks: Just 0 }
+                , children:
+                    ( Node
+                        { value: { bytes: Just 296, entries: 0, individual: { alloc: 0.2, time: 0.0 }, inherited: { alloc: 1.0, time: 0.0 }, mod: "Main", name: "main", number: 2486, src: "app/Main.hs:(77,1)-(80,27)", ticks: Just 0 }
+                        , children:
+                            ( Node
+                                { value: { bytes: Just 0, entries: 1, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.6, time: 0.0 }, mod: "Main", name: "main.f", number: 2487, src: "app/Main.hs:79:5-13", ticks: Just 0 }
+                                , children:
+                                    ( Node
+                                        { value: { bytes: Just 0, entries: 1, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.6, time: 0.0 }, mod: "Main", name: "fib'", number: 2491, src: "app/Main.hs:(70,1)-(74,51)", ticks: Just 0 }
+                                        , children:
+                                            ( Node
+                                                { value: { bytes: Just 928, entries: 31, individual: { alloc: 0.6, time: 0.0 }, inherited: { alloc: 0.6, time: 0.0 }, mod: "Main", name: "fib'.go", number: 2492, src: "app/Main.hs:(72,5)-(74,51)", ticks: Just 0 }
+                                                , children: Nil
+                                                }
+                                            : Nil
+                                            )
+                                        }
+                                    : Nil
+                                    )
+                                }
+                            : Node
+                                { value: { bytes: Just 0, entries: 1, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.3, time: 0.0 }, mod: "Main", name: "main.g", number: 2488, src: "app/Main.hs:80:5-27", ticks: Just 0 }
+                                , children:
+                                    ( Node
+                                        { value: { bytes: Just 0, entries: 1, individual: { alloc: 0.0, time: 0.0 }, inherited: { alloc: 0.3, time: 0.0 }, mod: "Main", name: "fib'", number: 2489, src: "app/Main.hs:(70,1)-(74,51)", ticks: Just 0 }
+                                        , children:
+                                            ( Node
+                                                { value: { bytes: Just 448, entries: 16, individual: { alloc: 0.3, time: 0.0 }, inherited: { alloc: 0.3, time: 0.0 }, mod: "Main", name: "fib'.go", number: 2490, src: "app/Main.hs:(72,5)-(74,51)", ticks: Just 0 }
+                                                , children: Nil
+                                                }
+                                            : Nil
+                                            )
+                                        }
+                                    : Nil
+                                    )
+                                }
+                            : Nil
+                            )
+                        }
+                    : Nil
+                    )
+                }
+            : Node
+                { value: { bytes: Just 9928, entries: 0, individual: { alloc: 6.0, time: 0.0 }, inherited: { alloc: 6.0, time: 0.0 }, mod: "Main", name: "main", number: 2485, src: "app/Main.hs:(77,1)-(80,27)", ticks: Just 0 }
                 , children: Nil
                 }
             : Nil
